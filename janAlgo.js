9f71de8328869ff3293e3d04ae76a9b13223c7bd
elvaworkingstation <44248034+elvaworkingstation@users.noreply.github.com>
2021-01-19 21:32:27 -0800

    Update janAlgo.js

--- janAlgo.js
+++ janAlgo.js
@@ -51,3 +51,27 @@ var sortList = function(head) {
     };
     return head;
 };
+
+
+// Convert Sorted List to Binary Search Tree
+var sortedListToBST = function(head) {
+  		if(!head) return null;
+  		let arr = [];
+  		while(head != null){
+  			arr.push(head.val);
+      		head = head.next;
+  		}
+    
+    	var buildTree = function(left,right){
+        	if(left > right) return null;
+        	let mid = left + Math.floor( (right - left) / 2);
+        	let midVal = arr[mid];
+        	let node = new TreeNode(midVal);
+        	if(left == right) return node;
+        	node.left = buildTree(left, mid - 1);
+        	node.right = buildTree(mid + 1, right);
+        	return node;
+    	} 
+    	return buildTree(0,arr.length - 1);
+};
+
